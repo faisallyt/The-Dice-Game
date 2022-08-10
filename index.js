5e5b29abec48a26e2dff3e44ecccea9dc7e7b7d9
Faisal Dilshad <96649676+faisallyt@users.noreply.github.com>
2022-08-10 16:25:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var random1=Math.floor(Math.random()*6)+1;
-var random2=Math.floor(Math.random()*6)+1;
-var randomimage1="images/dice"+random1+".png";
-var randomimage2="images/dice"+random2+".png";
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomimage1);
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomimage2);
-if(random1==random2){
-  document.querySelector("h2").innerHTML="Match Drawn.";
+var random1;
+var random2;
+function function1(){
+  random1=Math.floor(Math.random()*6)+1;
+  var randomimage1="images/dice"+random1+".png";
+  var image1=document.querySelectorAll("img")[0];
+  image1.setAttribute("src",randomimage1);
+    document.querySelector("h2").innerHTML="Player 2's Chance";
 }
-else if(random1>random2){
-  document.querySelector("h2").innerHTML="Player 1 won the Match ";
+function function2(){
+  random2=Math.floor(Math.random()*6)+1;
+  var randomimage2="images/dice"+random2+".png";
+  var image2=document.querySelectorAll("img")[1];
+  image2.setAttribute("src",randomimage2);
+  if(random1==random2){
+    document.querySelector("h2").innerHTML="Match Drawn.";
+  }
+  else if(random1>random2){
+    document.querySelector("h2").innerHTML="Player 1 won the Match ";
+  }
+  else if(random1<random2){
+    document.querySelector("h2").innerHTML="Player 2 Won the Match ";
+  }
 }
-else if(random1<random2){
-  document.querySelector("h2").innerHTML="Player 2 Won the Match ";
-}
+document.querySelectorAll("img")[0].addEventListener("click",function1);
+
+document.querySelectorAll("img")[1].addEventListener("click",function2);
