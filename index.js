adb446f51c4fbfc1fefacf24e3b5f142f87d1454
Faisal Dilshad <96649676+faisallyt@users.noreply.github.com>
2022-08-11 18:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var random1;
 var random2;
 function function1(){
   random1=Math.floor(Math.random()*6)+1;
-  var randomimage1="images/dice"+random1+".png";
+  var randomimage1="dice"+random1+".png";
   var image1=document.querySelectorAll("img")[0];
   image1.setAttribute("src",randomimage1);
     document.querySelector("h2").innerHTML="Player 2's Chance";
 }
 function function2(){
   random2=Math.floor(Math.random()*6)+1;
-  var randomimage2="images/dice"+random2+".png";
+  var randomimage2="dice"+random2+".png";
   var image2=document.querySelectorAll("img")[1];
   image2.setAttribute("src",randomimage2);
   if(random1==random2){
